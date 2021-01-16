6f5ff52c60f9cbb51555055c00fc5ffb02e2311e
Adam Green <adam.green@kcl.ac.uk>
2021-01-16 19:23:54 +0000

    editted exports again

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,14 @@
 export {default as Watermark} from './Watermark.svelte';
-import {default as Line} from './Line';
-import {default as Graph} from './Graph';
-import {default as Workings} from './Workings.svelte';
-import {default as Session} from './Session.svelte';
+export {default as Line} from './Line';
+export {default as Graph} from './Graph';
+export {default as Workings} from './Workings.svelte';
+export {default as Session} from './Session.svelte';
+
+import Watermark from './Watermark.svelte';
+import Line from './Line';
+import Graph from './Graph';
+import Workings from './Workings.svelte';
+import Session from './Session.svelte';
 
 export default {
     Graph,
