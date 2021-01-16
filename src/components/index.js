8e6cd84f7c03694964f54cce7e389df7bfac9e73
Adam Green <adam.green@kcl.ac.uk>
2021-01-16 21:25:46 +0000

    updated the Tryal UI library to include new components

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,18 @@ export {default as Line} from './Line';
 export {default as Graph} from './Graph';
 export {default as Workings} from './Workings.svelte';
 export {default as Question} from './Question';
+export {default as Image} from './Image.svelte';
+export {default as Text} from './Text.svelte';
+export {default as List} from './List.svelte';
 
 import Watermark from './Watermark.svelte';
 import Line from './Line';
 import Graph from './Graph';
 import Workings from './Workings.svelte';
 import Question from './Question';
+import Image from './Image.svelte';
+import Text from './Text.svelte';
+import List from './List.svelte';
 
 export default {
     Graph,
@@ -16,4 +22,7 @@ export default {
     Watermark,
     Workings,
     Question,
+    Image,
+    Text,
+    List,
 }
