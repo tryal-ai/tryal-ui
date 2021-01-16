c20ea4646b82cb6bdf4be666499baf498346c106
Adam Green <adam.green@kcl.ac.uk>
2021-01-16 19:10:01 +0000

    now logging the components themselves

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,14 @@ import Line from './Line';
 import Graph from './Graph';
 import Workings from './Workings.svelte';
 import Session from './Session.svelte';
-console.log("Finished loading components...");
-export default {
+export const components = {
     Graph,
     Line,
     Session,
     Watermark,
     Workings,
 }
+
+console.log(components);
+
+export default components;
