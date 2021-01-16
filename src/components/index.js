5dbed004ff77a89c8b035bc4730d207dd3ab316b
Adam Green <adam.green@kcl.ac.uk>
2021-01-16 19:08:04 +0000

    added tiny console.log statement to coax a response

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import Line from './Line';
 import Graph from './Graph';
 import Workings from './Workings.svelte';
 import Session from './Session.svelte';
+console.log("Finished loading components...");
 export default {
     Graph,
     Line,
