f5eead5d29cd698e94baea197ae294e1a0c60e3e
Adam Green <adam.green@kcl.ac.uk>
2021-03-24 15:04:11 +0000

    updated explicit multiplication to not include end times

--- src/components/Expression/types/explicitMultiplication.svelte
+++ src/components/Expression/types/explicitMultiplication.svelte
@@ -4,7 +4,7 @@
 
 {#each components as c, i}
     <svelte:component this={c.component} {...c.props}></svelte:component>
-    {#if i != components.length}
+    {#if i != components.length - 1}
         <math-o>{@html '&#x00d7;'}</math-o>
     {/if}
 {/each}
