6756fe8e12c1ead713dc01e6b1cf6883676b18e7
Adam Green <adam.green@kcl.ac.uk>
2021-03-31 09:53:38 +0100

    updated to correspond to correct spelling

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,7 @@ export {default as Image} from './Image.svelte';
 export {default as Text} from './Text.svelte';
 export {default as List} from './List.svelte';
 export {default as Multichoice} from './Multichoice.svelte';
-export {parser} from 'trygrammar';
+export {parse} from 'trygrammar';
 
 import Watermark from './Watermark.svelte';
 import Line from './Line';
@@ -18,7 +18,7 @@ import Image from './Image.svelte';
 import Text from './Text.svelte';
 import List from './List.svelte';
 import Multichoice from './Multichoice.svelte';
-import {parser} from 'trygrammar';
+import {parse} from 'trygrammar';
 
 export default {
     Graph,
@@ -30,5 +30,5 @@ export default {
     Text,
     List,
     Multichoice,
-    parser,
+    parse,
 }
